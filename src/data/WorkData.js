cf9131838a47b9097af4fd77610eb622da3c5d59
Samruddhi Patil. <samruddhip746@gmail.com>
2024-08-07 11:54:03 +0530

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+import bookvault from "../assets/ProjectImages/Thebookvault.png"
+import apiFetching from "../assets/ProjectImages/ecommerce.png"
+import resume from "../assets/ProjectImages/resumebuilder.png"
+import expense from "../assets/ProjectImages/expensetracker.png"
+import tributepage from "../assets/ProjectImages/tributepage.png"
+import multistepform from "../assets/ProjectImages/multistepform.png"
+export const Work = [
+    {
+        id: 1,
+        name: "Ecommerce Website",
+        description: "It is a MERN Stack Online Book Store - ThemIndia BookStore, with an admin page to view customer details.",
+        tags: ["MongoDB", "Express.js", "React.js", "Node.js"],
+        image: bookvault,
+        demo: "https://online-bookstore-mern.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/Mern-Project"
+    },
+    {
+        id: 2,
+        name: "Resume Builder",
+        description: "A Resume Builder website built in ReactJS with editing and downloading features.",
+        tags: ["React"],
+        image: resume,
+        demo: "https://samruddhipatil-react-resume-builder.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/React-Resume-Builder"
+    },
+    {
+        id: 3,
+        name: "API Fetching",
+        description: "A project demonstrating API fetching using HTML, CSS, and JavaScript.",
+        tags: ["HTML", "CSS", "JavaScript"],
+        image: apiFetching,
+        demo: "https://samruddhipatil-api-fetching.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/API-Fetching"
+    },
+    {
+        id: 4,
+        name: "Expense Tracker",
+        description: "An Expense Tracker website which uses your local storage as a database and keeps track of your expenses.",
+        tags: ["react", "styledComponents", "react-router"],
+        image: expense,
+        demo: "https://samruddhipatil-expense-management.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/Expense-Management"
+    },
+    {
+        id: 5,
+        name: "Tribute Page",
+        description: "A simple tribute page created using HTML and CSS.",
+        tags: ["HTML", "CSS"],
+        image: tributepage,
+        demo: "https://samruddhipatil-tribute-page.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/Tribute-Page"
+    },
+    {
+        id: 6,
+        name: "Multistep Form",
+        description: "A multistep form built with React and styled-components, featuring form validation and dynamic steps.",
+        tags: ["React", "styled-components", "Form Validation"],
+        image: multistepform,
+        demo: "https://samruddhipatil-multi-step-form-with-progress-bar.vercel.app/",
+        github: "https://github.com/PatilSamruddhi02/Multi-step-Form-with-Progress-Bar"
+    }
+];
